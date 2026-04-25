5e4ef4fb56fb980cc64b1418447d6a4f699871c8
Claudio Larrea <investigacion@uccuyo.edu.ar>
2026-04-25 14:55:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -231,23 +231,11 @@ p, label, span {
 """, unsafe_allow_html=True)
 
 st.markdown(
-    """
-    <div class='header-uccuyo' style='
-        background: linear-gradient(90deg, #0b5d4b, #177e6c);
-        padding: 30px;
-        border-radius: 15px;
-        margin-bottom: 30px;
-        width: 100%;
-    '>
-
-        <h1 style="margin:0;">Universidad Católica de Cuyo</h1>
-
-        <h2 style="margin-top:10px;">Secretaría de Investigación</h2>
-
-        <h3 style="margin-top:5px;">Consejo de Investigación</h3>
-
-    </div>
-    """,
+    """<div class="header-uccuyo" style="background: linear-gradient(90deg, #0b5d4b, #177e6c); padding: 30px; border-radius: 15px; margin-bottom: 30px; width: 100%;">
+<h1 style="margin:0;">Universidad Católica de Cuyo</h1>
+<h2 style="margin-top:10px;">Secretaría de Investigación</h2>
+<h3 style="margin-top:5px;">Consejo de Investigación</h3>
+</div>""",
     unsafe_allow_html=True
 )
 
